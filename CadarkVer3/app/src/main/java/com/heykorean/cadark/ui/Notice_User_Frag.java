b4567cfc0b2ebf37d7c98a8675b9d1abb43d69f7
Cadark <hey.hung.vn@gmail.com>
2016-02-25 14:47:29 +0700

    Update Tab Slide

---
+++ CadarkVer3/app/src/main/java/com/heykorean/cadark/ui/Notice_User_Frag.java
@@ -0,0 +1,22 @@
+package com.heykorean.cadark.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.heykorean.cadark.R;
+
+/**
+ * Created by Hey.Hung on 2/25/2016.
+ */
+public class Notice_User_Frag extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.notification_user_frag_layout, container, false);
+        return v;
+    }
+}

b65b2d15abc72f29114295a3c205bae40937852e
Cadark <hey.hung.vn@gmail.com>
2016-03-01 16:23:33 +0700

    Car detail

---
+++ CadarkVer3/app/src/main/java/com/heykorean/cadark/TestAct.java
@@ -0,0 +1,15 @@
+package com.heykorean.cadark;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Hey.Hung on 3/1/2016.
+ */
+public class TestAct extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_car);
+    }
+}
